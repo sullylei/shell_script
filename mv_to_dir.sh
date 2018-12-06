3549709828eb55cb239eebe830384cf2a6fb70dd
javaer <sullylei0207@gmail.com>
2018-12-07 07:36:09 +0800

    Update mv_to_dir.sh
    1.添加umask创建文件或目录的权限
    2.该shell脚本支持对多个目录进行处理，可查看transferDir
    3.在查找处理的文件时，过滤对日志的处理，可查看transferFiles

--- mv_to_dir.sh
+++ mv_to_dir.sh
@@ -1,54 +1,61 @@
 #!/bin/bash
-transferDir=/home/sully/move_test
+umask22
+transferDir="/home/sully/move_test,/tmp"
 
 #transfer day
 dayCount=3
-
-#touch transfer log
-transferLog=${transferDir}/transfer_to_dir_success.log
-if [ ! -f "${transferLog}}" ]
-then
-        touch ${transferLog}
-        chmod 777 ${transferLog}
-fi
 #控制日期
-timeStamp=`date -d "3 days" +%s`
-cd ${transferDir}
-#当前时间
-startTime=`date +'%Y-%m-%d %H:%M:%S'`
-#打印开始时间
-echo "${startTime} start transfer Dir:${transferDir}" >>${transferLog}
-startSeconds=$(date --date="${startTime}" +%s);
-#查询需处理的文件
-transferFiles=`find ${transferDir} -maxdepth 1 -type f -mtime +${dayCount}`
-#逐个文件处理
-for transferFile in ${transferFiles}
+expirationDate=`date -d "3 days" +%s`
+
+array=({transferDirs//,/ })
+for transferDir in ${array[@]}
 do
-        modifyDate=`stat "${transferFile}"|awk '$1=="Modify:"{print $2}'|awk -F "-" '{print $1""$2""$3}'`
-        echo ${transferFile}    
-        #get modifyTime second
-        modifyDate1=`ls --full-time "${transferFile}"|awk '{print $6}'`
-        modifyTime=`ls --full-time "${transferFile}"|awk '{print $7}'|awk -F "." '{print $1}'`
-        modifyDateSecond=`date -d "${modifyDate1} ${modifyTime}" +%s`
-
-        #check modify time
-        if [ ${modifyDateSecond} -gt ${timeStamp} ]
+        #touch transfer log
+        transferLog=${transferDir}/transfer_to_dir_success.log
+        if [ ! -f "${transferLog}}" ]
         then
-                echo ${modifyDateSecond}${timeStamp}
-                continue
+                touch ${transferLog}
+                chmod 777 ${transferLog}
         fi
 
-        #mkdir
-        if [ ! -d "${transferDir}/${modifyDate}/" ]
-        then
-                mkdir -p ${transferDir}/${modifyDate}/
-                chmod 774 ${transferDir}/${modifyDate}/
-        fi
+        cd ${transferDir}
+        #当前时间
+        startTime=`date +'%Y-%m-%d %H:%M:%S'`
+        #打印开始时间
+        echo "${startTime} start transfer Dir:${transferDir}" >>${transferLog}
+        startSeconds=$(date --date="${startTime}" +%s);
+        #查询需处理的文件
+        transferFiles=`find ${transferDir} -maxdepth 1 -type f ! -name "*.log" -mtime +${dayCount}`
+        #逐个文件处理
+        for transferFile in ${transferFiles}
+        do
+                modifyDate=`stat "${transferFile}"|awk '$1=="Modify:"{print $2}'|awk -F "-" '{print $1""$2""$3}'`
+                echo ${transferFile}    
+                #get modifyTime second
+                modifyDate1=`ls --full-time "${transferFile}"|awk '{print $6}'`
+                modifyTime=`ls --full-time "${transferFile}"|awk '{print $7}'|awk -F "." '{print $1}'`
+                modifyDateSecond=`date -d "${modifyDate1} ${modifyTime}" +%s`
 
-        #copy file
-        mv ${transferFile} ${transferDir}/${modifyDate}/
-        echo "move ${transferFile} success" >> ${transferLog}
+                #check modify time
+                if [ ${modifyDateSecond} -gt ${expirationDate} ]
+                then
+                        echo "${modifyDateSecond} 大于 ${expirationDate}"
+                        continue
+                fi
+
+                #mkdir
+                toDir=${transferDir}/${modifyDate}/
+                if [ ! -d "${toDir}" ]
+                then
+                        mkdir -p ${toDir}
+                        chmod 755 ${toDir}
+                fi
+
+                #copy file
+                mv ${transferFile} ${transferDir}/${modifyDate}/
+                echo "move ${transferFile} success" >> ${transferLog}
+        done
+        endTime=`date +'%Y-%m-%d %H:%M:%S'`
+        endSeconds=$(date --date="${endTime}" +%s);
+        echo "目录${transferDir}迁移到修改时间的文件夹，其运行时间：" $((endSeconds-startSeconds))"s">>${transferLog}
 done
-endTime=`date +'%Y-%m-%d %H:%M:%S'`
-endSeconds=$(date --date="${endTime}" +%s);
-echo "运行时间：" $((endSeconds-startSeconds))"s"
